2d62f196ce160a63d34bbf2cd4f72af69e7c1347
Mike Moran <mike@houseofmoran.com>
2016-02-27 22:59:13 +0000

    save summary to file

--- index.py
+++ index.py
@@ -2,9 +2,11 @@ from slacker import Slacker
 
 from optparse import OptionParser
 from sets import Set
+import json
 
 parser = OptionParser()
 parser.add_option("-t", "--token", dest="token", help="your slack token")
+parser.add_option("-o", "--out", dest="outfile", help="name of JSON file to write to")
 
 (options, args) = parser.parse_args()
 
@@ -16,10 +18,10 @@ channel_members = dict()
 
 for channel in response.body["channels"]:
     name = channel["name"]
-    #print name
+    # print name
     channel_members[name] = Set(channel["members"])
 
-print channel_members
+# print channel_members
 
 names_with_any_overlap = Set()
 overlaps = dict()
@@ -32,5 +34,11 @@ for (outer_name, outer_members) in channel_members.items():
                 names_with_any_overlap.add(inner_name)
                 overlaps[(outer_name, inner_name)] = overlap
 
-print names_with_any_overlap
-print overlaps
+# print names_with_any_overlap
+# print overlaps
+
+with open(options.outfile, 'w') as outfile:
+    summary = {
+        'names': list(names_with_any_overlap),
+        'overlaps': [("%s,%s" % channel_pair, len(members)) for (channel_pair, members) in overlaps.iteritems()]}
+    json.dump(summary,outfile)
