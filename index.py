48b05999246bc75a8b1d3729018fef806c485271
Mike Moran <mike@houseofmoran.com>
2016-02-27 22:39:33 +0000

    find overlap between channels, based on members

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
 from slacker import Slacker
 
 from optparse import OptionParser
+from sets import Set
 
 parser = OptionParser()
 parser.add_option("-t", "--token", dest="token", help="your slack token")
@@ -11,13 +12,25 @@ slack = Slacker(options.token)
 
 response = slack.channels.list()
 
-channel_users = dict()
+channel_members = dict()
 
 for channel in response.body["channels"]:
     name = channel["name"]
-    print name
-    channel_users[name] = channel["members"]
+    #print name
+    channel_members[name] = Set(channel["members"])
 
-print channel_users
+print channel_members
 
+names_with_any_overlap = Set()
+overlaps = dict()
+for (outer_name, outer_members) in channel_members.items():
+    for (inner_name, inner_members) in channel_members.items():
+        if outer_name < inner_name:
+            overlap = outer_members & inner_members
+            if len(overlap) > 1:
+                names_with_any_overlap.add(outer_name)
+                names_with_any_overlap.add(inner_name)
+                overlaps[(outer_name, inner_name)] = overlap
 
+print names_with_any_overlap
+print overlaps
