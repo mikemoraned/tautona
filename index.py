ee5cd74d4530f0de853661a920c7ac40b9e8e7e4
Mike Moran <mike@houseofmoran.com>
2016-02-28 00:21:49 +0000

    kinda not working as-is, because everyone is connected to everyone else

--- index.py
+++ index.py
@@ -17,28 +17,47 @@ slack = Slacker(options.token)
 response = slack.channels.list()
 
 channel_members = dict()
+member_channels = defaultdict(set)
 
 for channel in response.body["channels"]:
     name = channel["name"]
     # print name
-    channel_members[name] = Set(channel["members"])
+    members = Set(channel["members"])
+    channel_members[name] = members
+    for member in members:
+        member_channels[member].add(name)
 
 # print channel_members
 
 min_overlap = int(options.min_overlap)
-names_with_any_overlap = Set()
-overlaps = defaultdict(list)
-max_overlap = 0
+
+channel_names_with_any_overlap = Set()
+channel_overlaps = defaultdict(list)
+max_channel_overlap = 0
 for (outer_name, outer_members) in channel_members.items():
     for (inner_name, inner_members) in channel_members.items():
         if outer_name < inner_name:
             overlap = outer_members & inner_members
             overlap_size = len(overlap)
             if overlap_size >= min_overlap:
-                max_overlap = max(max_overlap, overlap_size)
-                names_with_any_overlap.add(outer_name)
-                names_with_any_overlap.add(inner_name)
-                overlaps[overlap_size].append((outer_name,inner_name))
+                max_channel_overlap = max(max_channel_overlap, overlap_size)
+                channel_names_with_any_overlap.add(outer_name)
+                channel_names_with_any_overlap.add(inner_name)
+                channel_overlaps[overlap_size].append((outer_name, inner_name))
+
+member_names_with_any_overlap = Set()
+member_overlaps = defaultdict(list)
+max_member_overlap = 0
+for (outer_name, outer_channels) in member_channels.items():
+    for (inner_name, inner_channels) in member_channels.items():
+        if outer_name < inner_name:
+            overlap = outer_channels & inner_channels
+            overlap_size = len(overlap)
+            if overlap_size >= min_overlap:
+                max_member_overlap = max(max_member_overlap, overlap_size)
+                member_names_with_any_overlap.add(outer_name)
+                member_names_with_any_overlap.add(inner_name)
+                member_overlaps[overlap_size].append((outer_name, inner_name))
 
 # print names_with_any_overlap
 # print overlaps
@@ -46,16 +65,16 @@ for (outer_name, outer_members) in channel_members.items():
 with open(options.outfile, 'w') as outfile:
     node_number = dict()
     nodes = list()
-    for name in names_with_any_overlap:
-        node = {'name': name}
+    for name in member_names_with_any_overlap:
+        node = {'name': "%s: %s" % (name, member_channels[name])}
         node_number[name] = len(nodes)
         nodes.append(node)
 
     links = list()
-    for (overlap, pairs) in overlaps.iteritems():
+    for (overlap, pairs) in member_overlaps.iteritems():
         for pair in pairs:
             (source, target) = pair
-            value = int(20 * (float(max_overlap - overlap) / float(max_overlap)))
+            value = int(20 * (float(max_member_overlap - overlap) / float(max_member_overlap)))
             links.append({
                 'source': node_number[source],
                 'target': node_number[target],
