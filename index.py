169759141bb7c8d725d9e34970552a3df8503e1a
Mike Moran <mike@houseofmoran.com>
2016-02-29 00:02:03 +0000

    use jacquard similarity, seems to work well

--- index.py
+++ index.py
@@ -6,7 +6,7 @@ import json
 
 parser = OptionParser()
 parser.add_option("-t", "--token", dest="token", help="your slack token")
-parser.add_option("-s", "--size", dest="min_overlap", help="minimum size of overlap")
+# parser.add_option("-s", "--size", dest="min_overlap", help="minimum size of overlap")
 parser.add_option("-o", "--out", dest="outfile", help="name of JSON file to write to")
 
 (options, args) = parser.parse_args()
@@ -19,40 +19,43 @@ channel_members = dict()
 
 for channel in response.body["channels"]:
     name = channel["name"]
-    channel_members[name] = set(channel["members"])
+    channel_members[name] = frozenset(channel["members"])
 
-min_overlap = int(options.min_overlap)
-names_with_any_overlap = set()
-overlaps = defaultdict(list)
-max_overlap = 0
+names = set()
+distances = defaultdict(list)
 for (outer_name, outer_members) in channel_members.items():
     for (inner_name, inner_members) in channel_members.items():
         if outer_name < inner_name:
-            overlap = outer_members & inner_members
-            overlap_size = len(overlap)
-            if overlap_size >= min_overlap:
-                max_overlap = max(max_overlap, overlap_size)
-                names_with_any_overlap.add(outer_name)
-                names_with_any_overlap.add(inner_name)
-                overlaps[overlap_size].append((outer_name,inner_name))
+            union_size = len(outer_members.union(inner_members))
+            if union_size >= 1:
+
+                similarity = \
+                    len(outer_members.intersection(inner_members)) \
+                    / union_size
+                distance = 1.0 - similarity
+
+                if distance < 0.5:
+                    names.add(outer_name)
+                    names.add(inner_name)
+                    bucket = float("{0:.2f}".format(distance))
+                    distances[bucket].append((outer_name, inner_name))
 
 with open(options.outfile, 'w') as outfile:
     node_number = dict()
     nodes = list()
-    for name in names_with_any_overlap:
+    for name in names:
         node = {'name': name}
         node_number[name] = len(nodes)
         nodes.append(node)
 
     links = list()
-    for (overlap, pairs) in overlaps.items():
+    for (distance, pairs) in distances.items():
         for pair in pairs:
             (source, target) = pair
-            value = int(20 * (float(max_overlap - overlap) / float(max_overlap)))
             links.append({
                 'source': node_number[source],
                 'target': node_number[target],
-                'value': value
+                'distance': (distance * 10.0)
             })
 
     summary = {
