05c019e1c317d854a3367299664dc7821b675784
Mike Moran <mike@houseofmoran.com>
2016-03-06 13:00:08 +0000

    first-cut of basic recommender, using avg distance of channels not joined yet

--- recommend.py
+++ recommend.py
@@ -3,6 +3,7 @@ from slacker import Slacker
 from optparse import OptionParser
 from collections import defaultdict
 import json
+from statistics import mean
 
 parser = OptionParser()
 parser.add_option("-i", "--in", dest="infile", help="the name of the summary JSON file")
@@ -27,8 +28,36 @@ def find_user_channel_names(userid):
     return frozenset(fetch())
 
 
+class Recommender():
+    def __init__(self, summary):
+        self.summary = summary
+
+    def recommend_channels(self, user_channel_set):
+        channel_distances = defaultdict(list)
+        for (distance, channel_pairs) in self.summary["distances"].items():
+            for channel_pair in channel_pairs:
+                channel_set = frozenset(channel_pair)
+                overlapping_channels = user_channel_set.intersection(channel_set)
+                missing_channels = channel_set.difference(overlapping_channels)
+                if len(missing_channels) > 0:
+                    for missing_channel in missing_channels:
+                        channel_distances[missing_channel].append(float(distance))
+
+        scored_channels = list()
+        for (channel_name, distances) in channel_distances.items():
+            score = 1.0 - mean(distances)
+            bucketed_score = float("{0:.2f}".format(score))
+            scored_channels.append({"name": channel_name, "score": bucketed_score})
+
+        return sorted(scored_channels, key=lambda i: i["score"], reverse=True)
+
+
 with open(options.infile, 'r') as infile:
     summary = json.load(infile)
+    recommender = Recommender(summary)
     user = slack.users.info(options.userid)
     user_channels = find_user_channel_names(options.userid)
     print(user_channels)
+    recommended_channels = recommender.recommend_channels(user_channels)
+    for recommended_channel in recommended_channels:
+        print(recommended_channel)
