4a0d8cba3e9bff2d0aa8190a242faf663bcce3b4
Mike Moran <mike@houseofmoran.com>
2016-03-22 00:34:05 +0000

    dump channel creation dates

--- ages.py
+++ ages.py
@@ -6,8 +6,6 @@ import json
 
 parser = OptionParser()
 parser.add_option("-t", "--token", dest="token", help="your slack token")
-parser.add_option("-d", "--distance", dest="max_distance", help="maximum distance between nodes (0 .. 1)")
-parser.add_option("-o", "--out", dest="outfile", help="name of JSON file to write to")
 
 (options, args) = parser.parse_args()
 
@@ -19,31 +17,5 @@ channel_members = dict()
 
 for channel in response.body["channels"]:
     name = channel["name"]
-    channel_members[name] = frozenset(channel["members"])
-
-names = set()
-distances = defaultdict(list)
-max_distance = float(options.max_distance)
-for (outer_name, outer_members) in channel_members.items():
-    for (inner_name, inner_members) in channel_members.items():
-        if outer_name < inner_name:
-            union_size = len(outer_members.union(inner_members))
-            if union_size >= 1:
-
-                similarity = \
-                    len(outer_members.intersection(inner_members)) \
-                    / union_size
-                distance = 1.0 - similarity
-
-                if distance < max_distance:
-                    names.add(outer_name)
-                    names.add(inner_name)
-                    bucket = float("{0:.2f}".format(distance))
-                    distances[bucket].append((outer_name, inner_name))
-
-with open(options.outfile, 'w') as outfile:
-    summary = {
-        'names': list(names),
-        'distances' : distances
-    }
-    json.dump(summary,outfile, sort_keys=True, indent=4, separators=(',', ': '))
+    created = channel["created"]
+    print("%s: %d" % (name, created))
