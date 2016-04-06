ac451a29d6ea534e5f7d835a55307720d9305976
Mike Moran <mike@houseofmoran.com>
2016-04-06 23:01:50 +0100

    extract age of newest message

--- ages.py
+++ ages.py
@@ -15,7 +15,19 @@ response = slack.channels.list()
 
 channel_members = dict()
 
+def newest_message(messages):
+    text_messages = [m for m in messages if m["type"] == "message" and "subtype" not in m]
+    by_ts = sorted(text_messages, key=lambda m : m["ts"], reverse=True)
+    if len(by_ts) > 0:
+        return by_ts[0]
+    else:
+        return None
+
 for channel in response.body["channels"]:
     name = channel["name"]
+    id = channel["id"]
+    history = slack.channels.history(id)
     created = channel["created"]
-    print("%s: %d" % (name, created))
+    newest = newest_message(history.body["messages"])
+    if newest is not None:
+        print("%s: %d -> %s: %s" % (name, created, newest["ts"], newest["text"]))
