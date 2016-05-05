3e553d0b829e7a32fcd77f938c8a5418c3527b0b
Mike Moran <mike@houseofmoran.com>
2016-05-05 23:21:31 +0100

    #1 pull all analysis + save/load into one place

--- analyse.py
+++ analyse.py
@@ -1,22 +1,11 @@
-from optparse import OptionParser
-from slacker import Slacker
+import logging
+from collections import defaultdict
 from gensim import corpora, models
 from gensim.models import tfidfmodel
-from collections import defaultdict
-import logging
-import json
-import re
+from optparse import OptionParser
 
 from channeltexts import ChannelTexts
 
-logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-
-parser = OptionParser()
-
-(options, args) = parser.parse_args()
-
-channel_texts = ChannelTexts.load("channel_text_id.json", "channel_texts.txt")
-texts = channel_texts.texts_only()
 
 def remove_single_occurrences(texts):
     frequency = defaultdict(int)
@@ -28,21 +17,53 @@ def remove_single_occurrences(texts):
              for text in texts]
     return texts
 
-texts = remove_single_occurrences(texts)
 
-dictionary = corpora.Dictionary(texts)
+class Analysed():
+    def __init__(self, dictionary, corpus, model):
+        self.dictionary = dictionary
+        self.corpus = corpus
+        self.model = model
 
-corpus = [dictionary.doc2bow(text) for text in texts]
-tfidf_model = tfidfmodel.TfidfModel(corpus, normalize=True)
-tfidf_corpus = tfidf_model[corpus]
+    def save(self):
+        self.dictionary.save('dictionary.dict')
+        corpora.MmCorpus.serialize('corpus.mm', self.corpus)
+        self.model.save("model_lsi")
 
-lsi = models.LsiModel(tfidf_corpus, id2word=dictionary, num_topics=400)
-lsi.print_topics(20)
+    @classmethod
+    def load(cls):
+        dictionary = corpora.Dictionary.load('dictionary.dict')
+        corpus = corpora.MmCorpus('corpus.mm')
+        lsi_model = models.LsiModel.load("model_lsi")
 
-model = lsi
+        print(dictionary)
+        print(corpus)
+        lsi_model.print_topics(20)
 
-dictionary.save('dictionary.dict')
-corpora.MmCorpus.serialize('corpus.mm', tfidf_corpus)
-model.save("model_lsi")
+        return Analysed(dictionary, corpus, lsi_model)
 
-print(dictionary)
+
+class Analyser():
+    def analyse(self, texts):
+        dictionary = corpora.Dictionary(texts)
+
+        corpus = [dictionary.doc2bow(text) for text in texts]
+
+        tfidf_model = tfidfmodel.TfidfModel(corpus, normalize=True)
+        tfidf_corpus = tfidf_model[corpus]
+
+        lsi_model = models.LsiModel(tfidf_corpus, id2word=dictionary, num_topics=400)
+
+        return Analysed(dictionary, tfidf_corpus, lsi_model)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+    parser = OptionParser()
+
+    (options, args) = parser.parse_args()
+
+    channel_texts = ChannelTexts.load("channel_text_id.json", "channel_texts.txt")
+    texts = remove_single_occurrences(channel_texts.texts_only())
+
+    Analyser().analyse(texts).save()
